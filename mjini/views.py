10962426d6c689ae20fc334c7ffdb7a60d77a51d
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-10-18 15:29:58 +0300

    profile view function added

--- mjini/views.py
+++ mjini/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from .models import *
+
+
 
 # Create your views here.
+def index(request):
+
+
+    return render(request,'index.html')
+
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.get(user=current_user)
+    print(profile)
+    # profile = Profile.objects.filter(user=request.user.id)
+    businesses = Business.objects.filter(owner = current_user)
+
+    return render(request, 'profile.html', {'profile': profile, 'businesses': businesses})
