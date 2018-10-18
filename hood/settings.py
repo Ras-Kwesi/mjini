b138ee4cf0d67b6230ab07613aea004c78110d08
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-10-18 12:43:13 +0300

    configurations of url and app in settings added

--- hood/settings.py
+++ hood/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qt$*ji2)s(jz!gp6q%xr4ldkb23k2o=$fm87^j_6731%e33-kf'
+SECRET_KEY = 'Kwesi1234'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mjini',
 ]
 
 MIDDLEWARE = [
