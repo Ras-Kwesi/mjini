869de39cca99dde2137babe10b4f6bb882c79ad8
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-10-22 15:57:52 +0300

    exit function

--- mjini/tests.py
+++ mjini/tests.py
@@ -21,3 +21,23 @@ class ProfileTest(TestCase):
         self.ras.save_user_profile(self.user)
         users = Profile.objects.all()
         self.assertTrue(len(users)>0)
+
+
+class HoodTest(TestCase):
+    def setUp(self):
+        self.user = User(username='Ras_Kwesi', email='ras@ras.com', password='passwadd')
+        self.user.save()
+        self.ras = Profile(bio='A python Programmer', contact='054234444', user=self.user)
+        self.hood = Hood(name = 'Ngong',bio = "Milimani",admin = self.user)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+        self.hood.delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.hood,Hood))
+
+    def test_save(self):
+        self.hood.save_hood()
+        hoods = Hood.objects.all()
+        self.assertTrue(len(hoods) == 1)
