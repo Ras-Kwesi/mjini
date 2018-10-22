01881d6f54c3911aa3050b2a10b43947e6f5f038
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-10-22 15:35:25 +0300

    profile test functions

--- mjini/tests.py
+++ mjini/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
+class ProfileTest(TestCase):
+    def setUp(self):
+        self.user = User(username = 'Ras_Kwesi', email = 'ras@ras.com', password = 'passwadd')
+        self.user.save()
+        self.ras = Profile(bio = 'A python Programmer',contact = '054234444', user = self.user)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.ras,Profile))
+
+    def test_save(self):
+        self.ras.create_user_profile(self.user,True)
+        self.ras.save_user_profile(self.user)
+        users = Profile.objects.all()
+        self.assertTrue(len(users)>0)
